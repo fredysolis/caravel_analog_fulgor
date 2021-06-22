4a584ae863b0e5fc53a92841f32f4101459591c0
Diego Hernando <djhernando96@gmail.com>
2021-06-22 18:32:17 -0300

    GDS DRC CLEAN wit PRECHECK LOCAL

---
+++ checks/user_analog_project_wrapper.magic.drc.tcl
@@ -0,0 +1,15 @@
+box 1800.590um 3168.225um 1800.760um 3168.395; feedback add "This layer can't abut or partially overlap between subcells" medium
+box 1800.590um 3168.285um 1800.760um 3168.480; feedback add "This layer can't abut or partially overlap between subcells" medium
+box 1798.140um 3163.250um 1798.205um 3163.305; feedback add "Metal1 > 3um spacing to unrelated m1 < 0.28um (met1.3b)" medium
+box 1798.205um 3163.255um 1798.465um 3163.305; feedback add "Metal1 > 3um spacing to unrelated m1 < 0.28um (met1.3b)" medium
+box 1798.205um 3163.250um 1798.270um 3163.255; feedback add "Metal1 > 3um spacing to unrelated m1 < 0.28um (met1.3b)" medium
+box 1798.270um 3163.250um 1798.440um 3163.255; feedback add "Metal1 > 3um spacing to unrelated m1 < 0.28um (met1.3b)" medium
+box 1798.440um 3163.250um 1798.465um 3163.255; feedback add "Metal1 > 3um spacing to unrelated m1 < 0.28um (met1.3b)" medium
+box 1798.465um 3163.250um 1799.505um 3163.305; feedback add "Metal1 > 3um spacing to unrelated m1 < 0.28um (met1.3b)" medium
+box 1800.820um 3163.300um 1801.150um 3163.305; feedback add "Metal1 > 3um spacing to unrelated m1 < 0.28um (met1.3b)" medium
+box 1800.770um 3163.250um 1800.835um 3163.300; feedback add "Metal1 > 3um spacing to unrelated m1 < 0.28um (met1.3b)" medium
+box 1800.835um 3163.255um 1801.095um 3163.300; feedback add "Metal1 > 3um spacing to unrelated m1 < 0.28um (met1.3b)" medium
+box 1800.835um 3163.250um 1800.900um 3163.255; feedback add "Metal1 > 3um spacing to unrelated m1 < 0.28um (met1.3b)" medium
+box 1800.900um 3163.250um 1801.070um 3163.255; feedback add "Metal1 > 3um spacing to unrelated m1 < 0.28um (met1.3b)" medium
+box 1801.070um 3163.250um 1801.095um 3163.255; feedback add "Metal1 > 3um spacing to unrelated m1 < 0.28um (met1.3b)" medium
+box 1801.095um 3163.250um 1801.165um 3163.300; feedback add "Metal1 > 3um spacing to unrelated m1 < 0.28um (met1.3b)" medium
